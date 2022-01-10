1dbf966f4a98dda5e484b836b944baa3820f0e0d
Ramona Gherman <rgherman9128@cosmoconsult.com>
2022-01-10 10:16:25 +0200

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int a = 1, b = 1, fib;
+            int n = ReadNumber("Introduceti numarul de elemente: ");
+
+            Console.Write($"Sirul Fibonacci este:");
+            Console.WriteLine();
+
+            Console.Write($" {a}, {b} , ");
+
+            for (int i = 3; i <= n; i++)
+            {
+                fib = a + b;
+                a = b;
+                b = fib;
+                Console.Write($"{fib}, ");
+            }
+
+            return 0;
+        }
+        static int ReadNumber(string label)
+        {
+            Console.Write(label);
+            string value = Console.ReadLine();
+            int valueAsNumber = Convert.ToInt32(value);
+            return valueAsNumber;
+        }
+    }
+}
